d9df77b22724fbcc77b0240b15007841e921981f
Sungu, Hasan Emrah <emrahsungu@hotmail.com>
2019-05-27 17:01:40 +0900

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,6 +3,12 @@
 set -e
 run_cmd="dotnet run --server.urls http://*:80"
 
+# starting sshd process
+
+sed -i "s/SSH_PORT/$SSH_PORT/g" /etc/ssh/sshd_config
+
+/usr/sbin/sshd
+
 
 >&2 echo "SQL Server is up - executing command"
 exec $run_cmd
