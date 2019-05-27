91e3f509c2e01d078496bd763f6eec1b127b9514
Sungu, Hasan Emrah <emrahsungu@hotmail.com>
2019-05-27 17:13:20 +0900

    Add files via upload

---
+++ init_container.sh
@@ -0,0 +1,39 @@
+#!/usr/bin/env bash
+
+cat >/etc/motd <<EOL 
+
+  _____                               
+
+  /  _  \ __________ _________   ____  
+
+ /  /_\  \\___   /  |  \_  __ \_/ __ \ 
+
+/    |    \/    /|  |  /|  | \/\  ___/ 
+
+\____|__  /_____ \____/ |__|    \___  >
+
+        \/      \/                  \/ 
+
+A P P   S E R V I C E   O N   L I N U X
+
+
+
+Documentation: http://aka.ms/webapp-linux
+
+Dotnet quickstart: https://aka.ms/dotnet-qs
+
+.NETCore runtime version: `ls -X /usr/share/dotnet/shared/Microsoft.NETCore.App | tail -n 1`
+
+
+
+EOL
+
+cat /etc/motd
+
+
+
+# starting sshd process
+
+sed -i "s/SSH_PORT/$SSH_PORT/g" /etc/ssh/sshd_config
+
+/usr/sbin/sshd
