9dfce99186592efa2085a5feb95913836aec279a
Sungu, Hasan Emrah <emrahsungu@hotmail.com>
2019-05-27 19:30:45 +0900

    Update init_container.sh

--- init_container.sh
+++ init_container.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 run_cmd="dotnet run --server.urls http://*:80"
 # starting sshd process
-sed -i "s/SSH_PORT/$SSH_PORT/g" /etc/ssh/sshd_config
+sed -i "s/SSH_PORT/$SSH_PORT/g"
+/usr/sbin/sshd
 /etc/ssh/sshd
 >&2 echo "SQL Server is up - executing command"
 exec $run_cmd
