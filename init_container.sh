af17cb34e87f8cb4c7573d47b0d2dded80828f5a
Sungu, Hasan Emrah <emrahsungu@hotmail.com>
2019-05-27 19:49:10 +0900

    Update init_container.sh

--- init_container.sh
+++ init_container.sh
@@ -1,9 +1,5 @@
 #!/bin/bash
-
-
-
 cat >/etc/motd <<EOL 
-
   _____                               
 
   /  _  \ __________ _________   ____  
@@ -18,24 +14,14 @@ cat >/etc/motd <<EOL
 
 A P P   S E R V I C E   O N   L I N U X
 
-
-
 Documentation: http://aka.ms/webapp-linux
-
 Dotnet quickstart: https://aka.ms/dotnet-qs
-
 .NETCore runtime version: `ls -X /usr/share/dotnet/shared/Microsoft.NETCore.App | tail -n 1`
-
-
-
 EOL
-
 cat /etc/motd
 # starting sshd process
 sed -i "s/SSH_PORT/$SSH_PORT/g" /etc/ssh/sshd_config
 /usr/sbin/sshd
-
-
 run_cmd="dotnet run --server.urls http://*:80"
 >&2 echo "SQL Server is up - executing command"
 exec $run_cmd
