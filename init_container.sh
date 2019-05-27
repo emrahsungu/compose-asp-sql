01daf077f53ead6edaf4701c30a647ecce72dbe6
Sungu, Hasan Emrah <emrahsungu@hotmail.com>
2019-05-27 19:51:26 +0900

    Update init_container.sh

--- init_container.sh
+++ init_container.sh
@@ -1,19 +1,12 @@
 #!/bin/bash
-cat >/etc/motd <<EOL 
-  _____                               
-
+cat >/etc/motd <<EOL
+_____                               
   /  _  \ __________ _________   ____  
-
  /  /_\  \\___   /  |  \_  __ \_/ __ \ 
-
-/    |    \/    /|  |  /|  | \/\  ___/ 
-
+    |    \/    /|  |  /|  | \/\  ___/ 
 \____|__  /_____ \____/ |__|    \___  >
-
         \/      \/                  \/ 
-
 A P P   S E R V I C E   O N   L I N U X
-
 Documentation: http://aka.ms/webapp-linux
 Dotnet quickstart: https://aka.ms/dotnet-qs
 .NETCore runtime version: `ls -X /usr/share/dotnet/shared/Microsoft.NETCore.App | tail -n 1`
